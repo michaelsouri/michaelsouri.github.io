28cc11009da2cd3b64718c5b0adc0839649374d9
Michael Souri <michaelsouri@csu.fullerton.edu>
2022-08-24 02:11:03 -0700

    initial commit - intro.js

---
+++ js/intro.js
@@ -0,0 +1,43 @@
+let header = document.querySelector("#intro");
+let animation = [
+  { t: " ", ms: 200 },
+  { t: "_", ms: 200 },
+  { t: " ", ms: 200 },
+  { t: "_", ms: 200 },
+  { t: "m_", ms: 100 },
+  { t: "mi_", ms: 100 },
+  { t: "mic_", ms: 100 },
+  { t: "mich_", ms: 100 },
+  { t: "micha_", ms: 100 },
+  { t: "michae_", ms: 100 },
+  { t: "michael_", ms: 100 },
+  { t: "michael _", ms: 100 },
+  { t: "michael s_", ms: 100 },
+  { t: "michael so_", ms: 100 },
+  { t: "michael sou_", ms: 100 },
+  { t: "michael sour_", ms: 100 },
+  { t: "michael souri_", ms: 100 },
+  { t: "michael souri", ms: 300 },
+];
+let stepDenominator = 1;
+if (window.localStorage.stepDenominator)
+  stepDenominator = window.localStorage.stepDenominator;
+let i = 0;
+let update = () => {
+  let step = animation[i];
+  header.innerText = step.t;
+  i++;
+
+  if (i < animation.length) setTimeout(update, step.ms / stepDenominator);
+  else {
+    header.classList.add("top");
+    setTimeout(() => {
+      /* TODO 
+        document.getElementById("main").style.opacity = 1;
+        tesseract();
+        */
+    }, 500);
+    window.localStorage.stepDenominator = 2;
+  }
+};
+update();
