f64b7e7129d846f33d60b6273a6ec60996d198fa
Michael Souri <michaelsouri@csu.fullerton.edu>
2022-08-27 07:42:49 -0700

    project-data.js commit

---
+++ js/project-data.js
@@ -0,0 +1,14 @@
+const ProjectData = {
+  diff: {
+    title: "diff",
+    github: "https://github.com/michaelsouri/diff",
+    description:
+      "my personal version of the unix-like operating system command with custom output options. it compares the contents of two seperate files and writes to the standard output the list of changes necessary to convert one file into the other.",
+  },
+  portfolio: {
+    title: "portfolio",
+    github: "https://github.com/michaelsouri/michaelsouri.github.io",
+    description:
+      "this is the portfolio. went for something unique yet aesthetically pleasing. utilized <b>svg</b> animations.",
+  },
+};
