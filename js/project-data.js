02d26845af3ac47af9191733100968083bd86419
Michael Souri <michaelsouri@csu.fullerton.edu>
2022-09-08 06:58:31 -0700

    updated project-data.js

--- js/project-data.js
+++ js/project-data.js
@@ -9,6 +9,12 @@ const ProjectData = {
     title: "portfolio",
     github: "https://github.com/michaelsouri/michaelsouri.github.io",
     description:
-      "this is the portfolio. went for something unique yet aesthetically pleasing. utilized <b>svg</b> animations.",
+      "you are currently viewing the portfolio. went for something unique yet aesthetically pleasing. overall, i am happy with how it turned out. utilized <b>svg</b> animations.",
+  },
+  compiler: {
+    title: "custom compiler",
+    github: "https://github.com/michaelsouri/custom-compiler",
+    description:
+      "challenging university project that taught me a lot about compilers. translates a specialized program with specific grammar into a <b>high-level</b> language (<b>c++</b>).",
   },
 };
