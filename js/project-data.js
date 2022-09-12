9f96667903e4cefcca3b22d75072d8e0ef7cb88c
Michael Souri <michaelsouri@csu.fullerton.edu>
2022-09-11 23:19:05 -0700

    added proj

--- js/project-data.js
+++ js/project-data.js
@@ -9,7 +9,13 @@ const ProjectData = {
     title: "portfolio",
     github: "https://github.com/michaelsouri/michaelsouri.github.io",
     description:
-      "you are currently viewing the portfolio. went for something unique yet aesthetically pleasing. overall, i am happy with how it turned out. utilized <b>svg</b> animations.",
+      "you are currently viewing the portfolio. went for something unique yet aesthetically pleasing. overall, i am happy with how everything turned out. the <b>svg</b> animations especially.",
+  },
+  encryption: {
+    title: "encrypted password manager",
+    github: "https://github.com/michaelsouri/encrypted-password-manager",
+    description:
+      "created a <b>full-stack</b> simple encrypted password manager with <b>react.js</b>, <b>node.js</b>, and <b>mysql</b>. once the user enters their password (and associated service), the system encrypts the password (aes 256) to the back-end (mysql database) then decrypts and displays the password when prompted to with the front-end.",
   },
   compiler: {
     title: "custom compiler",
